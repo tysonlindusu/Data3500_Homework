3c33848114282c490bf4a98ab4d69a0675875f7c
root <root@ip-172-31-6-102.us-west-1.compute.internal>
2025-05-10 15:52:04 +0000

    completed activity 6

---
+++ Week 2/Activity 6/activity6.py
@@ -0,0 +1,9 @@
+"""
+Programming Activity 6
+
+Write a program that prints all the multiples of 5, from 5 to 95 using a for loop. 
+"""
+
+for i in range(5, 95):
+    if i % 5 == 0:
+        print(i)
